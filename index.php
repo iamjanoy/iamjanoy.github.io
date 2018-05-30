c4f32ea158697ede0fc501fd7569b2623ba2c5c9
jrintong94@gmail.com <jrintong94@gmail.com>
2018-05-30 11:20:11 +0800

    1st upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+
+
+$xml=simplexml_load_file("https://item.rakuten.co.jp/drescco/c/0000000574/");
+print_r($xml);
+ ?>
---
+++ test.php
@@ -0,0 +1,17 @@
+<form name=form_mirine24_order method=post action="https://www.mainmast24.com:40000/remorder.aspx" target="_blank">
+	<input type="hidden" name="UserID" value="ecircle">
+	<input type="hidden" name="OrderSt" value="0">
+	<input type="hidden" name="UserOrderNo" value="0541231">
+	<input type="hidden" name="CustNm" value="testing">
+	<input type="hidden" name="CustKanaNm" value="<?=$mirinecustkananm?>">
+	<input type="hidden" name="Zip" value="49221-9">
+	<input type="hidden" name="Addr1" value="432 Raisin River Dr Adrian, Michigan ">
+	<input type="hidden" name="Addr2" value="">
+	<input type="hidden" name="Addr3" value="">
+	<input type="hidden" name="Country" value="United States">
+	<input type="hidden" name="TelNo" value="5177592419">
+	<input type="hidden" name="Email" value="hyperkornkid@gmail.com">
+	<input type="hidden" name="Items" value="MI Cara  Blue">
+	<input type="hidden" name="Note" value="<?=$mirinenote?>">
+	<input type="submit" name="submit" value="submit">
+</form>
